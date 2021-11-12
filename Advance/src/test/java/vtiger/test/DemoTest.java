538ec8f773a97c6569f6e0ac7ae0e24effa2efe9
abhil <abhil@LAPTOP-A42B5ES7>
2021-11-12 14:37:23 +0530

    1st commit

---
+++ Advance/src/test/java/vtiger/test/DemoTest.java
@@ -0,0 +1,11 @@
+package vtiger.test;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+   @Test
+   public void dispTest()
+   {
+	   System.out.println("Smoke test");
+   }
+}
---
+++ Advance/src/test/java/vtiger/test/Sample.java
@@ -0,0 +1,11 @@
+package vtiger.test;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+   @Test
+   public void samTest()
+   {
+	   System.out.println("Regression");
+   }
+}
