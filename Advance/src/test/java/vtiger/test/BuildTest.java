f999e7d675d097bff28a2eb06d9b42ea541f08c6
abhil <abhil@LAPTOP-A42B5ES7>
2021-11-12 17:03:50 +0530

    Build

---
+++ Advance/src/test/java/vtiger/test/BuildTest.java
@@ -0,0 +1,11 @@
+package vtiger.test;
+
+import org.testng.annotations.Test;
+
+public class BuildTest {
+   @Test 
+   public void buildtest()
+   {
+	   System.out.println("build creation");
+   }
+}
